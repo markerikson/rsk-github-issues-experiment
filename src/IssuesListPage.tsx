7e1daa7603965f887e8d4751a179767f65a8f746
Mark Erikson <mark@isquaredsoftware.com>
2019-05-24 21:06:30 -0400

    Basic TS and formatting

--- src/IssuesListPage.tsx
+++ src/IssuesListPage.tsx
@@ -4,12 +4,18 @@ import { render } from "react-dom";
 
 const RAILS_API_URL = "https://api.github.com/repos/rails/rails/issues?per_page=25&page=1";
 
+interface Issue {
+    id: number;
+    title: string;
+    number: number;
+}
+
 export const IssuesListPage = () => {
-    const [issues, setIssues] = useState<any>([]);
+    const [issues, setIssues] = useState<Issue[]>([]);
 
     useEffect(() => {
         async function fetchIssues() {
-            const result = await axios.get(RAILS_API_URL);
+            const result = await axios.get<Issue[]>(RAILS_API_URL);
             console.log("Result: ", result);
 
             setIssues(result.data);
@@ -18,7 +24,7 @@ export const IssuesListPage = () => {
         fetchIssues();
     }, []);
 
-    const renderedIssues = issues.map((issue: any) => (
+    const renderedIssues = issues.map(issue => (
         <li>
             # {issue.number}: {issue.title}
         </li>
