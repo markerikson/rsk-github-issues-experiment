58fb692a21c890aa9362721b4515b1a0eff3f2e7
Mark Erikson <mark@isquaredsoftware.com>
2019-06-12 15:00:06 +0300

    Initial store setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { configureStore } from "redux-starter-kit";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import repoDetailsReducer from "./features/RepoSearch/repoDetails";
+import issuesDisplayReducer from "./features/IssuesDisplay/issuesDisplay";
+
+const store = configureStore({
+    reducer: {
+        repoDetails: repoDetailsReducer,
+        issuesDisplay: issuesDisplayReducer,
+    },
+});
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
