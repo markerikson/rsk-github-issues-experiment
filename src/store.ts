baa5867e5f02cda0a9e7d6da896f92cbd47187f9
Mark Erikson <mark@isquaredsoftware.com>
2019-06-13 07:31:08 +0300

    Add issues state

--- src/store.ts
+++ src/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "redux-starter-kit";
 
 import repoDetailsReducer from "./features/RepoSearch/repoDetails";
 import issuesDisplayReducer from "./features/IssuesDisplay/issuesDisplay";
+import issuesReducer from "./features/IssuesList/issues";
 
 export const store = configureStore({
     reducer: {
         repoDetails: repoDetailsReducer,
         issuesDisplay: issuesDisplayReducer,
+        issues: issuesReducer,
     },
 });
 
